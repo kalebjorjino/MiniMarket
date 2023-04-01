d4440236bf92ccfff96d84792a3c4df7f90b5110
aleaxim <56270967+aleaxim@users.noreply.github.com>
2023-04-02 02:59:57 +0800

    add footer

--- resources/views/storefront/footer.blade.php
+++ resources/views/storefront/footer.blade.php
@@ -3,35 +3,36 @@
     <div class="container ">
         <div class="row mb-5">
             <div class="col-md-5">
-                <a href="/" class="footer-site-logo"><img src="{{ asset('images/logo/samj-logo.png') }}" height="75px"></a>
+                <a href="/" class="footer-site-logo"><img src="{{ asset('images/logo/samj-logo.png') }}"
+                        width="200"></a>
                 <p>When Korean tradition meeets modern convenience.</p>
             </div>
             <div class="col-md-2 ml-auto">
-                <h3>OFFERINGS</h3>
+                <h3>SAMJ KMART</h3>
                 <ul class="list-unstyled links">
-                    <li><a href="/shop" style="color: #DDAB54">Product Catalog</a></li>
-                    {{-- <li><a href="#" style="color: #DDAB54">Request Design</a></li> --}}
-                    <li><a href="/booking/services" style="color: #DDAB54">Services</a></li>
-                    <li><a href="/book-now" style="color: #DDAB54">Book Now</a></li>
+                    <li><a href="/menu">Menu</a></li>
+                    <li><a href="/menu/categories">Categories</a></li>
+                    <li><a href="/menu/brands">Brands</a></li>
                 </ul>
             </div>
             <div class="col-md-2 ml-auto">
                 <h3>RESOURCES</h3>
                 <ul class="list-unstyled links">
-                    <li><a href="/about-us" style="color: #DDAB54">About Us</a></li>
-                    <li><a href="/contact-us" style="color: #DDAB54">Contact Us</a></li>
-                    <li><a href="/faq" style="color: #DDAB54">FAQs</a></li>
+                    <li><a href="/about-us">About</a></li>
+                    <li><a href="/contact-us">Contact Us</a></li>
+                    <li><a href="/account/dashboard">Account</a></li>
+                    {{-- <li><a href="#">FAQs</a></li> --}}
                 </ul>
             </div>
             <div class="col-md-2 ml-auto">
                 <h3>GET THE APP</h3>
                 <ul class="list-unstyled links">
-                    <li><a href="#" class="dl-btn-grad">Download Dong App</a></li>
+                    <li><a href="#" class="dl-btn-grad">SAMJ KMART App</a></li>
                 </ul>
             </div>
         </div>
 
-        <div class="row d-flex justify-content-between py-4 border-top">
+        <div class="row d-flex justify-content-between py-3 border-top">
             <div class="col">
                 <p class="mt-2">&copy; 2023 SAMJ Korean Mini Mart. All rights reserved.</p>
                 {{-- <ul class="list-unstyled link-menu nav-left">
