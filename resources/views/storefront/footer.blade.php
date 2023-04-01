40ddcf602f47ede337d34fc2727a87f0e0dd66a7
aleaxim <56270967+aleaxim@users.noreply.github.com>
2023-04-02 03:08:36 +0800

    fix footer

--- resources/views/storefront/footer.blade.php
+++ resources/views/storefront/footer.blade.php
@@ -1,57 +1,64 @@
-<footer class="footer-14398">
+  <section class="divider">
+      <div class="container">
+          <div class="row">
+              {{--  --}}
+          </div>
+      </div>
+  </section><!-- /.mail-chimp -->
+  <footer class="footer-14398">
 
-    <div class="container ">
-        <div class="row mb-5">
-            <div class="col-md-5">
-                <a href="/" class="footer-site-logo"><img src="{{ asset('images/logo/samj-logo.png') }}"
-                        width="200"></a>
-                <p>When Korean tradition meeets modern convenience.</p>
-            </div>
-            <div class="col-md-2 ml-auto">
-                <h3>SAMJ KMART</h3>
-                <ul class="list-unstyled links">
-                    <li><a href="/menu">Menu</a></li>
-                    <li><a href="/menu/categories">Categories</a></li>
-                    <li><a href="/menu/brands">Brands</a></li>
-                </ul>
-            </div>
-            <div class="col-md-2 ml-auto">
-                <h3>RESOURCES</h3>
-                <ul class="list-unstyled links">
-                    <li><a href="/about-us">About</a></li>
-                    <li><a href="/contact-us">Contact Us</a></li>
-                    <li><a href="/account/dashboard">Account</a></li>
-                    {{-- <li><a href="#">FAQs</a></li> --}}
-                </ul>
-            </div>
-            <div class="col-md-2 ml-auto">
-                <h3>GET THE APP</h3>
-                <ul class="list-unstyled links">
-                    <li><a href="#" class="dl-btn-grad">SAMJ KMART App</a></li>
-                </ul>
-            </div>
-        </div>
+      <div class="container ">
+          <div class="row mb-5">
+              <div class="col-md-5">
+                  <a href="/" class="footer-site-logo"><img src="{{ asset('images/logo/samj-logo.png') }}"
+                          width="200"></a>
+                  <p>When Korean tradition meeets modern convenience.</p>
+              </div>
+              <div class="col-md-2 ml-auto">
+                  <h3>SAMJ KMART</h3>
+                  <ul class="list-unstyled links">
+                      <li><a href="/menu">Menu</a></li>
+                      <li><a href="/menu/categories">Categories</a></li>
+                      <li><a href="/menu/brands">Brands</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-2 ml-auto">
+                  <h3>RESOURCES</h3>
+                  <ul class="list-unstyled links">
+                      <li><a href="/about-us">About</a></li>
+                      <li><a href="/contact-us">Contact Us</a></li>
+                      <li><a href="/account/dashboard">Account</a></li>
+                      {{-- <li><a href="#">FAQs</a></li> --}}
+                  </ul>
+              </div>
+              <div class="col-md-2 ml-auto">
+                  <h3>GET THE APP</h3>
+                  <ul class="list-unstyled links">
+                      <li><a href="#" class="dl-btn-grad">SAMJ KMART App</a></li>
+                  </ul>
+              </div>
+          </div>
 
-        <div class="row d-flex justify-content-between py-3 border-top">
-            <div class="col">
-                <p class="mt-2">&copy; 2023 SAMJ Korean Mini Mart. All rights reserved.</p>
-                {{-- <ul class="list-unstyled link-menu nav-left">
+          <div class="row d-flex justify-content-between py-3 border-top">
+              <div class="col">
+                  <p class="mt-2">&copy; 2023 SAMJ Korean Mini Mart. All rights reserved.</p>
+                  {{-- <ul class="list-unstyled link-menu nav-left">
             </ul> --}}
-            </div>
-            <div class="col ">
-                <ul class="list-unstyled d-flex social justify-content-end">
-                    <li><a class="link-dark"href="/privacy-policy">Privacy</a></li>
-                    <li><a class="link-dark"href="/terms-conditions">Terms</a></li>
-                    {{-- <li class="ms-3"><a class="link-dark"href="#">Code of Conduct</a></li> --}}
-                    <li><a class="link-dark" href="https://www.facebook.com/DongDigitalPhotoshop/" target="_blank"><i
-                                class="fa-brands fa-facebook fa-lg"></i></a>
-                    </li>
-                    <li><a class="link-dark" href="https://www.instagram.com/dongrelox/" target="_blank"><i
-                                class="fa-brands fa-instagram fa-lg"></i></a>
-                    </li>
-                </ul>
-            </div>
+              </div>
+              <div class="col ">
+                  <ul class="list-unstyled d-flex social justify-content-end">
+                      <li><a class="link-dark"href="/privacy-policy">Privacy</a></li>
+                      <li><a class="link-dark"href="/terms-conditions">Terms</a></li>
+                      {{-- <li class="ms-3"><a class="link-dark"href="#">Code of Conduct</a></li> --}}
+                      <li><a class="link-dark" href="https://www.facebook.com/DongDigitalPhotoshop/" target="_blank"><i
+                                  class="fa-brands fa-facebook fa-lg"></i></a>
+                      </li>
+                      <li><a class="link-dark" href="https://www.instagram.com/dongrelox/" target="_blank"><i
+                                  class="fa-brands fa-instagram fa-lg"></i></a>
+                      </li>
+                  </ul>
+              </div>
 
-        </div>
-    </div>
-</footer>
+          </div>
+      </div>
+  </footer>
